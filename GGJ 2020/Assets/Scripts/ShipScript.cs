9c884892bc1d34fb15b92d8c74870b580a875883
Samuel Fortune <s.fortune66@gmail.com>
2020-02-01 14:46:59 +1300

    Renaming ShipMovement to ShipScript

---
+++ GGJ 2020/Assets/Scripts/ShipScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class ShipScript : MonoBehaviour
+{
+    public GameObject core;
+    private Rigidbody2D thisRB;
+    public float health;
+
+    public float velocityMag;
+   
+    void Start()
+    {
+        health = 100.0f;
+        thisRB = core.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        velocityMag = thisRB.velocity.magnitude;
+
+        if (Input.GetKey("q"))
+        {
+            thisRB.AddTorque(10.0f);
+        }
+        if (Input.GetKey("e"))
+        {
+            thisRB.AddTorque(-10.0f);
+        }
+        if (Input.GetKey("w"))
+        {
+            Forwards();
+        }
+
+        if (Input.GetKey("s"))
+        {
+        }
+
+        if (Input.GetKey("a"))
+        {
+        }
+
+        if (Input.GetKey("d"))
+        {
+        }
+    }
+
+    void Forwards()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name.Contains("Thruster"))
+            {
+                child.GetComponent<ThrusterScript>().Thrust();
+            }
+        }
+    }
+
+    // 0 = forwards, 1 = backwards, 2 = left, 3 = right
+    void Move(int _direction)
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > 0.2f)
+        {
+            if (collision.gameObject.name.Contains("Thruster"))
+            {
+                health -= 2.0f;
+            }
+        }
+    }
+}
